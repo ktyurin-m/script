fe973bb6a3f8aa8e4aedca1e444a03c6de806f86
ktyurin-m <kirilltiutrin@gmail.com>
2023-08-25 23:12:56 +0700

    add function enter() for output text from data

--- script.sh
+++ script.sh
@@ -1,7 +1,36 @@
-#!/usr/bin/bash
+#!/bin/bash
 var1=$1
 var2=$2
-# echo $var1
-if [ "$var1" == "-a" ]; then
-    sed -i "13i $var2" main.tex
-fi
+#
+data_n() {
+    cat -n data
+}
+main_n() {
+    grep -n input main.tex 
+}
+count=$(wc -l data | cut -c1)
+count=$((count + 1)) 
+
+
+# if [ "$var1" == "-a" ]; then
+#     sed -i "$count i $var2" data
+# fi
+
+enter() {
+    while read line; do
+        echo "\input{../pack/$line.tex}"
+        if [ "$line" == "\tableofcontents" ]; then
+            echo "\tableofcontents"
+        fi
+
+        if [ "$line" == "\newpage" ]; then
+            echo "\newpage"
+        fi
+
+    done < data   
+}
+echo "\begin{document}" > test.tex 
+echo "\input{../settings/settings.tex}" >> test.tex
+enter >> test.tex
+echo "\end{document}" >> test.tex
+
